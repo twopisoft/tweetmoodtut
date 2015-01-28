4e37b7f887c4ffa1a1ce72e3df1217a431377684
twopisoft <twopisoft@gmail.com>
2015-01-28 09:54:55 +0800

    Initial Commit

---
+++ src/main/java/com/twopi/TweetMood/TweetMoodResource.java
@@ -0,0 +1,21 @@
+
+package com.twopi.TweetMood;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/")
+public class TweetMoodResource {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    @Path("get")
+    public String getIt() {
+        return "Hi there!";
+    }
+}
